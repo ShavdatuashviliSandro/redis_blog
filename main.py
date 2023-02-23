92edcd21f3d4a1a67628e2dc2028b4904eaeda2a
Sandro Shavdatuashvili <sandro.shav@gmail.com>
2023-02-23 15:32:49 +0400

    refactor html files / added post adding feature

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 import redis
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+r = redis.Redis()
+app.secret_key = 'sandro'
+last_id = 0
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        global last_id
+        req = request.form
+        name = req['full_name']
+        post = req['data']
+        last = r.get('last_id')
+        if last is None:
+            last_id = 1
+        else:
+            last_id = int(last)
+            last_id += 1
+
+        r.set(f"news:name:{last_id}", name)
+        r.set(f"news:post:{last_id}", post)
+        r.set('last_id', last_id)
+        r.lpush('post_id', last_id)
+        flash('Successfully submited the post', category='success')
+
     return render_template('home.html')
 
 
